f1ccfc037f9b1e918fc21581b6de32bbe232b837
Iza <izabelaoliveira033@gmail.com>
2026-08-30 19:28:45 -0300

    Add files via upload

---
+++ atividade 1/Exercicio1/ex1_qwen.py
@@ -0,0 +1,21 @@
+# requisitos: 1) pip install ollama
+#       2)  ollama run qwen2.5-coder
+from ollama import chat
+
+response = chat(
+    model="qwen2.5-coder",
+    messages=[
+        {
+            "role": "user",
+            "content": """Considere o seguinte código Java: 
+                         public String getUserInitials(String firstName, String lastName) {
+                              return firstName.substring(0, 1).toUpperCase()
+                                     + lastName.substring(0, 1).toUpperCase();
+                         }
+                         Tem algum bug nesse código? Algo que pode comprometer a funcionalidade dele? Caso sim, me aponte os problemas, e corrija o código por favor.
+                         """
+        }
+    ]
+)
+
+print(response["message"]["content"])
