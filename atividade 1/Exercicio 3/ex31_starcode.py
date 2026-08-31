eefd5199644900aae91b906479b3a6d7de36d277
Iza <izabelaoliveira033@gmail.com>
2026-08-30 21:34:41 -0300

    Add files via upload

---
+++ atividade 1/Exercicio 3/ex31_starcode.py
@@ -0,0 +1,24 @@
+# requisitos: 1) pip install ollama
+#       2)  ollama run qwen2.5-coder
+from ollama import chat
+
+response = chat(
+    model="starcoder2",
+    messages=[
+        {
+            "role": "user",
+            "content": """Considere o seguinte código Java: 
+                         ppublic double calculateAverage(List<Integer> grades) {
+    int sum = 0;
+    for (Integer grade : grades) {
+        sum += grade;
+    }
+    return sum / grades.size();
+}
+                         Gere testes automatizados (JUnit) para esse código, cobrindo casos normais e casos extremos, por favor.
+                         """
+        }
+    ]
+)
+
+print(response["message"]["content"])
---
+++ atividade 1/Exercicio 3/ex32_deepseek.py
@@ -0,0 +1,24 @@
+# requisitos: 1) pip install ollama
+#       2)  ollama run qwen2.5-coder
+from ollama import chat
+
+response = chat(
+    model="deepseek-coder",
+    messages=[
+        {
+            "role": "user",
+            "content": """Considere o seguinte código Java: 
+                         ppublic double calculateAverage(List<Integer> grades) {
+    int sum = 0;
+    for (Integer grade : grades) {
+        sum += grade;
+    }
+    return sum / grades.size();
+}
+                         Gere a documentação (JavaDoc) para esse método, por favor.
+                         """
+        }
+    ]
+)
+
+print(response["message"]["content"])
